dbc0b83feb03b514bbe5eb81c1f5f2bcdca9a582
Kritapas Suwannawin <kritapas.ms@gmail.com>
2025-03-06 19:57:00 +0700

    implement rate limit

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 
 import userController from '../controllers/userController';
 
@@ -6,8 +7,11 @@ import { validateAccessToken } from '../middlewares/userMiddleware';
 
 const router = express.Router();
 
+// Rete limit - 10 requests per minute, using access token as an identifier
+const rateLimiter = rateLimit({ windowMs: 60 * 1000, max: 10, keyGenerator: (req) => req.headers.authorization! });
+
 // Translation
-router.route('/translation').get(validateAccessToken, userController.getTranslation);
+router.route('/translation').get(validateAccessToken, rateLimiter, userController.getTranslation);
 
 // Language
 router.route('/language').get(userController.getLanguage);
